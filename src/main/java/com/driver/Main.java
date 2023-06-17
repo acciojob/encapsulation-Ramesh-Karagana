a923edf28fb05d9b4bdecd2057afb191b4a3b262
ramesh <ramesh@gmail.com>
2023-06-17 13:36:43 +0530

    added new code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    private String name;
+    //getter method for name
+    public String getName()
+    {
+        return name;
+    }
+    //setter method for name
+    public void setName(String name){
+        this.name=name;
+    }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
-public class RWOnly {
+public class RWOnly
+{
+    public static void main(String[] args)
+    {
+        Main s=new Main();
+        s.setName("Ramesh");
+        System.out.println(s.getName());
+    }
     
 }
